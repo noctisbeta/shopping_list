bd7d84360ec2a76acd0afb9c2a9bb9bb5c8fbc43
Janez Emeršič <noctisbeta@gmail.com>
2024-01-14 12:28:40 +0100

    comm

--- lib/dio_client.dart
+++ lib/dio_client.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 final dioClient = Dio(
   BaseOptions(
-    // baseUrl: 'http://localhost:8080',
-    baseUrl: 'https://shopping-list-backend-ca1w.onrender.com',
+    baseUrl: 'http://localhost:8080',
+    // baseUrl: 'https://shopping-list-backend-ca1w.onrender.com',
   ),
 );
